deaffa368be09e556f307b8ef0a0989576eee3c0
thiloi0509 <loinguyenthi716@gmail.com>
2021-09-14 21:58:52 +0700

    add new baitap

---
+++ Project1/1c.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <stdio.h>  
+using namespace std;
+
+void PTB2(double a, double b,double c) {
+   double d, x1, x2;
+	if (a == 0) {
+        {
+            cout << "phuong trinh vo nghiem" << endl;
+        }
+	}
+    else
+    {
+        d = b * b - 4 * a * c;
+        if (d < 0)
+        {
+            cout << "phuong trinh vo nghem" << endl;
+        }
+        else if (d == 0)
+        {
+            cout << "phuong trinh co nghiem kep x1=x2=" << -b / (2 * a) << endl;
+        }
+        else
+        {
+            x1 = (-b - sqrt(d)) / (2 * a);
+            x2 = (-b + sqrt(d)) / (2 * a);
+            cout << "phuong trinh co 2 ngiem phan biet:" << endl;
+            cout << "x1=" << x1 << endl;
+            cout << "x2=" << x2 << endl;
+        }
+    }
+}
+int main() {
+	double a, b ,c;
+	cout << "nhap a va b và c\n";
+	cin >> a >> b>>c;
+	PTB2(a, b,c);
+	return 0;
+}
